2775a90cf11701b8da425b31c241aea59a2db09b
juliolmuller <juliolmuller@outlook.com>
2025-02-06 23:20:26 -0300

    feat: create Proposals base structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +21,13 @@ class DatabaseSeeder extends Seeder
 
             Project::factory($randomProjectsPerUser)->create([
                 'created_by' => $user->id,
-            ]);
+            ])->each(function (Project $project) {
+                $randomProposalsPerProject = random_int(1, 20);
+
+                Proposal::factory($randomProposalsPerProject)->create([
+                    'project_id' => $project->id,
+                ]);
+            });
         });
     }
 }
